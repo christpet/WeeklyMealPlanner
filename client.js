21de9f5fcc3acb32030ff515a5baadc05a4d1a1b
christpet <32476396+christpet@users.noreply.github.com>
2022-11-22 07:38:16 +0100

    try again to make supabase work

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
 
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+
 export const supabase = createClient(
-  'https://xihitkzfvvvdvkcdvypc.supabase.co', 
-  ['SUPABASE_KEY']
+  supabaseUrl,
+  supabaseKey
 );
